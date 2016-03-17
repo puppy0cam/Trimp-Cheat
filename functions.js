8eb8c7e3400500843555446c61cdd3da420ba84b
puppy0cam <puppy0caminminecraft@gmail.com>
2016-03-17 13:11:59 +1000

    fully fixed heirloom storage integration
    warning against ruinTheFun() with heirloom storage active removed.

--- functions.js
+++ functions.js
@@ -1,12 +1,12 @@
 function fill(type, base) {
-    "use strict";
     if (!base) {
         if (game.heirlooms.Shield.storageSize.currentBonus > 0) {
             game.resources[type].owned = ((game.resources[type].max * (game.portal.Packrat.level * game.portal.Packrat.modifier)) * game.heirlooms.Shield.storageSize.currentBonus) + game.resources[type].max;
         } else {
             game.resources[type].owned = ((game.resources[type].max * (game.portal.Packrat.level * game.portal.Packrat.modifier)) + game.resources[type].max);
         };
-        if (game.heirlooms.Shield.storageSize.currentBonus > 0) {
+    } else {    
+		if (game.heirlooms.Shield.storageSize.currentBonus > 0) {
             game.resources[type].owned = ((game.resources[base].max * (game.portal.Packrat.level * game.portal.Packrat.modifier)) + game.resources[base].max * game.heirlooms.Shield.storageSize.currentBonus);
         } else {
             game.resources[type].owned = ((game.resources[base].max * (game.portal.Packrat.level * game.portal.Packrat.modifier)) + game.resources[base].max);
@@ -14,11 +14,9 @@ function fill(type, base) {
     }
 }
 function MapResource() {
-    "use strict";
     createMap(1 + game.stats.zonesCleared.value, "puppy0cam Caves", "Depths", 2.6, 25, 0.75, false, "puppy0cam Caves");
 }
 function ruinTheFun() {
-    "use strict";
     fill('food');
     fill('wood');
     fill('metal');
@@ -29,7 +27,6 @@ function ruinTheFun() {
     game.resources.trimps.owned = game.resources.trimps.realMax();
 }
 function buyStuff(thing) {
-    "use strict";
     buyBuilding(thing);
     ruinTheFun();
 }
