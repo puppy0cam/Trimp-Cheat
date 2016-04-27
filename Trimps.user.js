554e787c758764ec442423dfd3ec5a31f76d7549
puppy0cam <puppy0caminminecraft@gmail.com>
2016-04-27 12:22:10 +1000

    syntax fix

--- Trimps.user.js
+++ Trimps.user.js
@@ -13,6 +13,7 @@
 // @run-at       document-end
 // ==/UserScript==
 /* jshint -W097 */
+window.cheat = {};
 if (window.game !== undefined) {
 	window.cheat.fill = function (type, base) {
 		if (!base) {
@@ -20,7 +21,7 @@ if (window.game !== undefined) {
 				window.game.resources[type].owned = ((window.game.resources[type].max * (window.game.portal.Packrat.level * window.game.portal.Packrat.modifier)) * window.game.heirlooms.Shield.storageSize.currentBonus) + window.game.resources[type].max;
 			} else {
 				window.game.resources[type].owned = ((window.game.resources[type].max * (window.game.portal.Packrat.level * window.game.portal.Packrat.modifier)) + window.game.resources[type].max);
-			};
+			}
 		} else {
 			if (window.game.heirlooms.Shield.storageSize.currentBonus > 0) {
 				window.game.resources[type].owned = ((window.game.resources[base].max * (window.game.portal.Packrat.level * window.game.portal.Packrat.modifier)) + window.game.resources[base].max * window.game.heirlooms.Shield.storageSize.currentBonus);
@@ -28,10 +29,10 @@ if (window.game !== undefined) {
 				window.game.resources[type].owned = ((window.game.resources[base].max * (window.game.portal.Packrat.level * window.game.portal.Packrat.modifier)) + window.game.resources[base].max);
 			}
 		}
-	}
+	};
 	window.cheat.mapResource = function () {
 		window.createMap(1 + window.game.stats.zonesCleared.value, "puppy0cam Caves", "Depths", 2.6, 25, 0.75, false, "puppy0cam Caves");
-	}
+	};
 	window.cheat.ruinTheFun = function () {
 		window.cheat.fill('food');
 		window.cheat.fill('wood');
@@ -41,11 +42,11 @@ if (window.game !== undefined) {
 		window.cheat.fill('gems', 'food');
 		window.cheat.fill('helium', 'food');
 		window.game.resources.trimps.owned = window.game.resources.trimps.realMax();
-	}
+	};
 	window.cheat.buyStuff = function (thing) {
 		window.buyBuilding(thing);
 		window.cheat.ruinTheFun();
-	}
+	};
 	window.cheat.heirloomFinding = function () {
 		window.game.heirlooms.rarityBreakpoints[8] = 200;
 		window.game.heirlooms.rarities[9] = [];
@@ -56,17 +57,17 @@ if (window.game !== undefined) {
 		window.game.heirlooms.rarities[9][4] = 6000;
 		window.game.heirlooms.rarities[9][5] = 3000;
 		window.game.heirlooms.rarities[9][6] = 1000;
-	}
+	};
 	window.cheat.sellHeirloom = function () {
-		if (window.game.global.heirloomsExtra[0] == undefined) {
+		if (window.game.global.heirloomsExtra[0] === undefined) {
 			window.message("ALERT: you do not have an heirloom to sell!", "Notices");
 		} else {
 			window.selectHeirloom(window.game.global.heirloomsExtra.length - 1, 'heirloomsExtra', this);
 			window.recycleHeirloom(true);
 		}
-	}
+	};
 	window.extraHeirlooms.innerHTML = extraHeirlooms.innerHTML + '<div id="sellHeirlooms" class="noselect heirloomBtnActive heirBtn" onclick="window.cheat.sellHeirloom()">Sell one</div>';
-	delete window.addItem
+	delete window.addItem;
 }
 GM_registerMenuCommand('Create Ideal Map', function () {
 	window.cheat.mapResource();
